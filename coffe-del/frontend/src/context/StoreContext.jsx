dd4df67395be28a70ee8547dd4d148184ce5f9ab
Sollimba <134264168+Sollimba@users.noreply.github.com>
2024-05-18 20:00:20 +0300

    frontsign

--- coffe-del/frontend/src/context/StoreContext.jsx
+++ coffe-del/frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
@@ -7,6 +7,8 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   
   const [cartItems,setCartItems] = useState({});
+  const url ="http://localhost:4000"
+  const [token,setToken] = useState("");
 
   const addToCart = (itemId) =>{
     if (!cartItems[itemId]){
@@ -38,7 +40,10 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromeCart,
-    getTotalCartAmount
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
   }
   return (
     <StoreContext.Provider value={contextValue}>
