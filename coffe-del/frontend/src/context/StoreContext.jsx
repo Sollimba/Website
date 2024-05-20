f4bfe48b631b8f6ff4b84336cdc00bb93ba9d998
Sollimba <134264168+Sollimba@users.noreply.github.com>
2024-05-20 18:11:09 +0300

    FINELLI

--- coffe-del/frontend/src/context/StoreContext.jsx
+++ coffe-del/frontend/src/context/StoreContext.jsx
@@ -11,17 +11,23 @@ const StoreContextProvider = (props) => {
   const [token,setToken] = useState("");
   const [food_list,setFoodlist] =useState([])
 
-  const addToCart = (itemId) =>{
+  const addToCart = async (itemId) =>{
     if (!cartItems[itemId]){
       setCartItems((prev)=>({...prev,[itemId]:1}))
     }
     else {
       setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
     }
+    if (token) {
+      await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+    }
   }
 
-  const removeFromeCart = (itemId) => {
+  const removeFromeCart =  async (itemId) => {
     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    if (token) {
+      await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+    }
   }
 
   const getTotalCartAmount = () => {
@@ -40,11 +46,17 @@ const fetchFoodList = async () =>{
   setFoodlist(response.data.data)
 }
 
+const loadCartData = async (token)=>{
+  const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+  setCartItems(response.data.cartData);
+}
+
 useEffect(()=>{
   async function loadData() {
     await fetchFoodList();
     if (localStorage.getItem("token")){
       setToken(localStorage.getItem("token"));
+      await loadCartData(localStorage.getItem("token"));
     }
   }
   loadData();
